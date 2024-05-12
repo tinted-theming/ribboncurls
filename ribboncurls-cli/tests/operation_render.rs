a86309d5e22d1129856bdfb2a03812c3d2db1888
Jamy Golden <code@jamygolden.com>
2024-05-12 20:28:22 +0200

    Fix test binary path

--- ribboncurls-cli/tests/operation_render.rs
+++ ribboncurls-cli/tests/operation_render.rs
@@ -4,7 +4,7 @@ use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
 
-const COMMAND_PATH: &str = "../target/debug/ribboncurls";
+const COMMAND_PATH: &str = "../target/release/ribboncurls";
 const SUBCOMMAND_PATH: &str = "render";
 
 #[test]
