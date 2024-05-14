3aafd4edb8a5bc65614cb148a509fd513329551e
Jamy Golden <code@jamygolden.com>
2024-05-15 00:18:15 +0200

    Add rendering

--- ribboncurls/src/main.rs
+++ ribboncurls/src/main.rs
@@ -1,11 +1,18 @@
+use std::collections::HashMap;
+
 fn main() {
+    let data = HashMap::from([
+        ("name".to_string(), "Tinted".to_string()),
+        ("surname".to_string(), "Theming".to_string()),
+        ("url".to_string(), ">> https://github.com/tinted-theming".to_string()),
+    ]);
     let template = r#"
-    Hello,  {{^section}}This is a section!{{/section}}! {{{name}}} {{surname}}. {{#some-section}}This is a section!{{/some-section}}!"#;
-    let output = match ribboncurls::new_render(template) {
+    Hello,  {{^section}}This is a section!{{/section}}! {{{name}}} {{surname}}. {{#some-section}}This is a section!{{/some-section}}! - {{url}}"#;
+    let output = match ribboncurls::new_render(template, data) {
         Ok(res) => res,
         Err(err) => {
             println!("error: {:?}", err);
-            Vec::new()
+            String::new()
         }
     };
 
