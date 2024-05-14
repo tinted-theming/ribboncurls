2a9e0c1dd7514b5824d56b6374a83ce1744f6a03
Jamy Golden <code@jamygolden.com>
2024-05-14 23:38:03 +0200

    Create syntax tree

--- ribboncurls/src/main.rs
+++ ribboncurls/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    let template = r#"Hello, {{{name}}} {{surname}}. {{#some-section}}This is a section!{{/some-section}}!"#;
+    let template = r#"
+    Hello,  {{^section}}This is a section!{{/section}}! {{{name}}} {{surname}}. {{#some-section}}This is a section!{{/some-section}}!"#;
     let output = match ribboncurls::new_render(template) {
         Ok(res) => res,
         Err(err) => {
