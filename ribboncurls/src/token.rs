663293f8331f4944efd556c547e25ea8b9177be8
Jamy Golden <code@jamygolden.com>
2024-05-21 22:58:06 +0200

    Move syntax tree and tokens into module

---
+++ ribboncurls/src/token.rs
@@ -0,0 +1,107 @@
+use super::ParseContext;
+use super::RibboncurlsError;
+
+#[derive(Debug)]
+pub enum Token {
+    Text(String),
+    Variable(String),
+    EscapedVariable(String),
+    OpenSection(String),
+    CloseSection(String),
+    OpenInvertedSection(String),
+    // Partial(String),
+    Comment(String),
+    Delimiter((String, String)),
+}
+
+pub fn tokenize(template: &str, ctx: &mut ParseContext) -> Result<Vec<Token>, RibboncurlsError> {
+    let mut tokens = Vec::new();
+    let mut i = 0;
+
+    while i < template.len() {
+        let current_str = &template[i..];
+        let single_char_left_delimiter = &ctx.left_delimiter[0..1];
+        let left_delimiter_escape =
+            format!("{}{}", &ctx.left_delimiter, single_char_left_delimiter);
+
+        if current_str.starts_with(&left_delimiter_escape) {
+            let single_char_right_delimiter = &ctx.right_delimiter[0..1];
+            let right_delimiter_escape =
+                format!("{}{}", &ctx.right_delimiter, single_char_right_delimiter);
+
+            if let Some(end) = current_str.find(&right_delimiter_escape) {
+                let end = end + i; // index in `template`
+                let content = &template[i + left_delimiter_escape.len()..end].trim();
+
+                tokens.push(Token::Variable(content.to_string()));
+
+                i = end + right_delimiter_escape.len();
+            } else {
+                break;
+            }
+        } else if current_str.starts_with(&ctx.left_delimiter) {
+            // If there is a following end-delimiter
+            if let Some(end) = current_str[ctx.left_delimiter.len()..].find(&ctx.right_delimiter) {
+                let end = end + i + ctx.left_delimiter.len(); // index in `template`
+                let start_index = i + ctx.left_delimiter.len();
+                let right_delimiter_len = ctx.right_delimiter.len();
+
+                if start_index < template.len() && end < template.len() {
+                    let content = &template[start_index..end];
+
+                    if let Ok(token) = parse_tag(content, ctx) {
+                        tokens.push(token);
+                    }
+                }
+
+                i = end + right_delimiter_len;
+            } else {
+                break;
+            }
+        } else {
+            // Find the start of the next tag or end of the template
+            if let Some(next_tag_start) = current_str.find(&ctx.left_delimiter) {
+                let text = &template[i..i + next_tag_start];
+                if !text.is_empty() {
+                    tokens.push(Token::Text(text.to_string()));
+                }
+                i += next_tag_start;
+            // Otherwise add the remaining text
+            } else {
+                let text = &template[i..];
+                tokens.push(Token::Text(text.to_string()));
+                break;
+            }
+        }
+    }
+
+    Ok(tokens)
+}
+
+fn parse_tag(content: &str, ctx: &mut ParseContext) -> Result<Token, RibboncurlsError> {
+    match content.chars().next() {
+        Some('&') => Ok(Token::Variable(content[1..].trim().to_string())),
+        Some('#') => Ok(Token::OpenSection(content[1..].trim().to_string())),
+        Some('/') => Ok(Token::CloseSection(content[1..].trim().to_string())),
+        Some('^') => Ok(Token::OpenInvertedSection(content[1..].trim().to_string())),
+        // '>' => Some(Token::Partial(content[1..].trim().to_string())),
+        Some('!') => Ok(Token::Comment(content[1..].trim().to_string())),
+        Some('=') => {
+            let delimiters: Vec<&str> = content[1..content.len() - 1].trim().split(' ').collect();
+
+            match (delimiters.first(), delimiters.last()) {
+                (Some(left_delimiter), Some(right_delimiter)) => {
+                    ctx.left_delimiter = left_delimiter.to_string();
+                    ctx.right_delimiter = right_delimiter.to_string();
+
+                    Ok(Token::Delimiter((
+                        left_delimiter.to_string(),
+                        right_delimiter.to_string(),
+                    )))
+                }
+                _ => Err(RibboncurlsError::MissingDelimiter),
+            }
+        }
+        _ => Ok(Token::EscapedVariable(content.trim().to_string())),
+    }
+}
