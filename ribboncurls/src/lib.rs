b999de32aa3b0603444658b27322d037ec629bcb
Jamy Golden <code@jamygolden.com>
2024-05-25 11:36:58 +0200

    Add support for nested section scoped context

--- ribboncurls/src/lib.rs
+++ ribboncurls/src/lib.rs
@@ -32,7 +32,7 @@ pub fn render(
     data: &str,
     partials: Option<&str>,
 ) -> Result<String, RibboncurlsError> {
-    let data = serde_yaml::from_str(data)?;
+    let mut context_stack = vec![serde_yaml::from_str(data)?];
     let mut ctx = ParseContext {
         left_delimiter: DEFAULT_LEFT_DELIMITER.to_string(),
         right_delimiter: DEFAULT_RIGHT_DELIMITER.to_string(),
@@ -40,7 +40,7 @@ pub fn render(
     let tokens = tokenize(template, &mut ctx)?;
     let syntax_tree = create_syntax_tree(tokens)?;
 
-    render_syntax_tree(&syntax_tree, &data)
+    render_syntax_tree(&syntax_tree, &mut context_stack)
 }
 
 fn remove_leading_space(output: &mut String) {
@@ -61,7 +61,7 @@ fn remove_leading_line_and_space(output: &mut String) {
 
 fn render_syntax_tree(
     syntax_tree: &[SyntaxItem],
-    data: &Value,
+    context_stack: &mut Vec<Value>,
 ) -> Result<String, RibboncurlsError> {
     let mut output = String::new();
 
@@ -92,14 +92,14 @@ fn render_syntax_tree(
                 }
             }
             SyntaxItem::EscapedVariable(content) => {
-                if let Some(value) = data.get(content.as_str()) {
+                if let Some(value) = get_context_value(context_stack, content.as_str()) {
                     output.push_str(&html_escape::encode_text(&serde_yaml_value_to_string(
                         value,
                     )));
                 }
             }
             SyntaxItem::Variable(content) => {
-                if let Some(value) = data.get(content.as_str()) {
+                if let Some(value) = get_context_value(context_stack, content.as_str()) {
                     output.push_str(&serde_yaml_value_to_string(value));
                 }
             }
@@ -139,27 +139,47 @@ fn render_syntax_tree(
             } => {
                 remove_leading_space(&mut output);
 
-                match (data.get(name), inverted) {
+                let section_value_option = find_section_value(context_stack, name);
+                let mut is_mutating_context_stack = false;
+
+                if let Some(Value::Mapping(map)) = section_value_option {
+                    context_stack.push(Value::Mapping(map.clone()));
+
+                    is_mutating_context_stack = true;
+                }
+
+                let section_value_option = find_section_value(context_stack, name);
+
+                match (section_value_option, inverted) {
+                    (Some(Value::Mapping(_)), false) | (Some(Value::Mapping(_)), true) => {
+                        let section_output = render_syntax_tree(items, context_stack)?;
+
+                        output.push_str(&section_output);
+                    }
                     (Some(value), false) => {
-                        if !serde_yaml_value_to_string(value).is_empty() {
-                            let section_output = render_syntax_tree(items, data)?;
+                        if !serde_yaml_value_to_string(&value).is_empty() {
+                            let section_output = render_syntax_tree(items, context_stack)?;
 
                             output.push_str(&section_output);
-                        }
+                        };
                     }
                     (None, true) => {
-                        let section_output = render_syntax_tree(items, data)?;
+                        let section_output = render_syntax_tree(items, context_stack)?;
 
                         output.push_str(&section_output);
                     }
                     (Some(value), true) => {
-                        if serde_yaml_value_to_string(value).is_empty() {
-                            let section_output = render_syntax_tree(items, data)?;
+                        if serde_yaml_value_to_string(&value).is_empty() {
+                            let section_output = render_syntax_tree(items, context_stack)?;
 
                             output.push_str(&section_output);
                         }
                     }
                     (None, false) => {}
+                }
+
+                if is_mutating_context_stack {
+                    context_stack.pop();
                 }
             }
         };
@@ -181,5 +201,77 @@ fn serde_yaml_value_to_string(value: &Value) -> String {
         }
 
         _ => "".to_string(),
+    }
+}
+
+fn find_section_value(context_stack: &[Value], section_name: &str) -> Option<Value> {
+    for context in context_stack.iter().rev() {
+        if let Some(name) = context.get(section_name) {
+            return Some(name.clone());
+        }
+    }
+
+    None
+}
+
+fn get_context_value<'a>(context_stack: &'a [Value], path: &str) -> Option<&'a Value> {
+    if path.is_empty() || path.is_empty() {
+        return None;
+    }
+
+    if path == "." {
+        if let Some(root_context) = context_stack.first() {
+            return Some(root_context);
+        }
+    }
+
+    let parts = path.split('.').collect::<Vec<&str>>();
+
+    for context in context_stack.iter().rev() {
+        if parts.len() == 1 {
+            return match context {
+                Value::Mapping(map) => map.get(path),
+                _ => None,
+            };
+        } else if let Value::Mapping(map) = context {
+            let first_part = parts.first()?;
+
+            if let Some(map) = map.get(first_part) {
+                return get_value(map, &parts[1..].join("."));
+            } else {
+                return None;
+            };
+        }
+    }
+
+    None
+}
+
+fn get_value<'a>(data: &'a Value, path: &str) -> Option<&'a Value> {
+    if path.is_empty() || path.is_empty() {
+        return None;
+    }
+
+    if path == "." {
+        return Some(data);
+    }
+
+    let parts = path.split('.').collect::<Vec<&str>>();
+
+    if parts.len() == 1 {
+        return data.get(path);
+    }
+
+    match data {
+        Value::Mapping(map) => {
+            let first_part = parts.first()?;
+
+            if let Some(map) = map.get(first_part) {
+                get_value(map, &parts[1..].join("."))
+            } else {
+                None
+            }
+        }
+        _ => None,
     }
 }
